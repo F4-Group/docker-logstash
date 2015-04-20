e656c188cfc01e9db9b36ca9a5e368a3337cc889
Charly Koza <cka@f4-group.com>
2015-04-20 17:56:58 +0200

    trying ES_JAVA_OPTS

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ ES_HOST=${ES_HOST:-127.0.0.1}
 
 # configure elasticsearch in kibana
 sed -i "s/\s.elasticsearch:.*/     elasticsearch: \"http:\/\/$ES_HOST:$ES_HTTP_PORT\",/g" /opt/logstash/vendor/kibana/config.js
-/opt/logstash/bin/logstash agent -f /opt/logstash.conf -- web
+/opt/logstash/bin/logstash $ES_JAVA_OPTS agent -f /opt/logstash.conf -- web
