32c35c0d2990bb3c47f89985144724296085fede
Ludovic Perrine <lpe@f4-group.com>
2015-12-08 15:24:03 +0100

    Use default Logstash heap size

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 ES_HOST=${ES_HOST:-127.0.0.1}
 ES_HTTP_PORT=${ES_HTTP_PORT:-9200}
 ES_PORT=${ES_PORT:-9300}
-LS_HEAP_SIZE=${LS_HEAP_SIZE:-24g}
 
 sed "s/%ES_CONF%/\n    embedded => false\n    host => [\"$ES_HOST\"]\n    port => $ES_HTTP_PORT\n    protocol => \"http\"\n/g" /opt/logstash.conf.template > /opt/logstash.conf
 
