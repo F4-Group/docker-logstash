af77369cb4c73f285e619c0f11cb3fce6db63d1e
Ludovic Perrine <lpe@f4-group.com>
2015-12-08 18:31:03 +0100

    Added back LS_HEAP_SIZE

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ES_HOST=${ES_HOST:-127.0.0.1}
 ES_HTTP_PORT=${ES_HTTP_PORT:-9200}
+export LS_HEAP_SIZE=${LS_HEAP_SIZE:-24g}
 
 sed "s/%ES_CONF%/\n    embedded => false\n    host => [\"$ES_HOST\"]\n    port => $ES_HTTP_PORT\n    protocol => \"http\"\n/g" /opt/logstash.conf.template > /opt/logstash.conf
 
