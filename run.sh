912d185670598bcc1c224e88318d4edaca8936a0
Ludovic Perrine <lpe@f4-group.com>
2015-12-08 18:59:19 +0100

    Removed unused kibana config change

--- run.sh
+++ run.sh
@@ -5,6 +5,4 @@ export LS_HEAP_SIZE=${LS_HEAP_SIZE:-24g}
 
 sed "s/%ES_CONF%/\n    embedded => false\n    host => [\"$ES_HOST\"]\n    port => $ES_HTTP_PORT\n    protocol => \"http\"\n/g" /opt/logstash.conf.template > /opt/logstash.conf
 
-# configure elasticsearch in kibana
-sed -i "s/\s.elasticsearch:.*/     elasticsearch: \"http:\/\/$ES_HOST:$ES_HTTP_PORT\",/g" /opt/logstash/vendor/kibana/config.js
 /opt/logstash/bin/logstash agent -f /opt/logstash.conf -l /dev/null -- web
