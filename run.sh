998644000797451e87174dd472fb148cd0e0e63c
Ludovic Perrine <lpe@f4-group.com>
2015-12-09 15:18:44 +0100

    Logs
    (cherry picked from commit f42a74b718c4677ec104045755c93ff18a0932df)

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ export LS_HEAP_SIZE=${LS_HEAP_SIZE:-24g}
 
 sed "s/%ES_CONF%/\n    embedded => false\n    host => [\"$ES_HOST\"]\n    port => $ES_HTTP_PORT\n    protocol => \"http\"\n/g" /opt/logstash.conf.template > /opt/logstash.conf
 
-/opt/logstash/bin/logstash agent -f /opt/logstash.conf -l /dev/null -- web
+/opt/logstash/bin/logstash agent -f /opt/logstash.conf
