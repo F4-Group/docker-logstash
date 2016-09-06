60c73de830b6cae5c74883d92f9b0cbdf096ab04
Charly Koza <cka@f4-group.com>
2016-09-06 15:54:58 +0200

    updated conf for 2.4

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ ES_HOST=${ES_HOST:-127.0.0.1}
 ES_HTTP_PORT=${ES_HTTP_PORT:-9200}
 export LS_HEAP_SIZE=${LS_HEAP_SIZE:-24g}
 
-sed "s/%ES_CONF%/\n    embedded => false\n    host => [\"$ES_HOST\"]\n    port => $ES_HTTP_PORT\n    protocol => \"http\"\n/g" /opt/logstash.conf.template > /opt/logstash.conf
+sed "s/%ES_CONF%/\n    embedded => false\n    hosts => [\"$ES_HOST":"$ES_HTTP_PORT\"]\n/g" /opt/logstash.conf.template > /opt/logstash.conf
 
 /opt/logstash/bin/logstash agent -f /opt/logstash.conf
