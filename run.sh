70dbb004af77ad2d915c81dbdd6a787bb4e3b1ff
Ludovic Perrine <lpe@f4-group.com>
2019-01-14 18:10:46 +0100

    Configurable pipeline batch size

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@
 ES_HOSTS=${ES_HOSTS:-127.0.0.1:9200}
 ES_HOSTS=[\"${ES_HOSTS//,/\",\"}\"]
 WORKERS=${WORKERS:-8}
+PIPELINE_BATCH_SIZE=${PIPELINE_BATCH_SIZE:-125}
 export LS_HEAP_SIZE=${LS_HEAP_SIZE:-24g}
 
 sed -e "s/%ES_HOSTS%/$ES_HOSTS/g" \
     -e "s/%WORKERS%/$WORKERS/g" \
     /opt/logstash.conf.template > /opt/logstash.conf
 
-/opt/logstash/bin/logstash agent -w $WORKERS -f /opt/logstash.conf
+/opt/logstash/bin/logstash agent -w $WORKERS -b $PIPELINE_BATCH_SIZE -f /opt/logstash.conf
